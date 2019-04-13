f8cb5657803a8fed8a7575825e3c73924057ec1a
Gustavo <gvdambros@gmail.com>
2019-04-13 18:01:07 +0000

    One to many relationship

--- book_shop.sql
+++ book_shop.sql
@@ -12,22 +12,22 @@ CREATE TABLE books
  
 INSERT INTO books (title, author_fname, author_lname, released_year, stock_quantity, pages)
 VALUES
-('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291),
-('Norse Mythology', 'Neil', 'Gaiman',2016, 43, 304),
-('American Gods', 'Neil', 'Gaiman', 2001, 12, 465),
-('Interpreter of Maladies', 'Jhumpa', 'Lahiri', 1996, 97, 198),
-('A Hologram for the King: A Novel', 'Dave', 'Eggers', 2012, 154, 352),
-('The Circle', 'Dave', 'Eggers', 2013, 26, 504),
-('The Amazing Adventures of Kavalier & Clay', 'Michael', 'Chabon', 2000, 68, 634),
-('Just Kids', 'Patti', 'Smith', 2010, 55, 304),
-('A Heartbreaking Work of Staggering Genius', 'Dave', 'Eggers', 2001, 104, 437),
-('Coraline', 'Neil', 'Gaiman', 2003, 100, 208),
-('What We Talk About When We Talk About Love: Stories', 'Raymond', 'Carver', 1981, 23, 176),
-("Where I'm Calling From: Selected Stories", 'Raymond', 'Carver', 1989, 12, 526),
-('White Noise', 'Don', 'DeLillo', 1985, 49, 320),
-('Cannery Row', 'John', 'Steinbeck', 1945, 95, 181),
-('Oblivion: Stories', 'David', 'Foster Wallace', 2004, 172, 329),
-('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, 343);
+    ('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291),
+    ('Norse Mythology', 'Neil', 'Gaiman',2016, 43, 304),
+    ('American Gods', 'Neil', 'Gaiman', 2001, 12, 465),
+    ('Interpreter of Maladies', 'Jhumpa', 'Lahiri', 1996, 97, 198),
+    ('A Hologram for the King: A Novel', 'Dave', 'Eggers', 2012, 154, 352),
+    ('The Circle', 'Dave', 'Eggers', 2013, 26, 504),
+    ('The Amazing Adventures of Kavalier & Clay', 'Michael', 'Chabon', 2000, 68, 634),
+    ('Just Kids', 'Patti', 'Smith', 2010, 55, 304),
+    ('A Heartbreaking Work of Staggering Genius', 'Dave', 'Eggers', 2001, 104, 437),
+    ('Coraline', 'Neil', 'Gaiman', 2003, 100, 208),
+    ('What We Talk About When We Talk About Love: Stories', 'Raymond', 'Carver', 1981, 23, 176),
+    ("Where I'm Calling From: Selected Stories", 'Raymond', 'Carver', 1989, 12, 526),
+    ('White Noise', 'Don', 'DeLillo', 1985, 49, 320),
+    ('Cannery Row', 'John', 'Steinbeck', 1945, 95, 181),
+    ('Oblivion: Stories', 'David', 'Foster Wallace', 2004, 172, 329),
+    ('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, 343);
 
 INSERT INTO books
     (title, author_fname, author_lname, released_year, stock_quantity, pages)
@@ -48,3 +48,59 @@ INSERT INTO books
             ELSE '***'
         END AS STOCK
     FROM books; 
+
+
+CREATE TABLE customers(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100)
+);
+
+CREATE TABLE orders(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_date DATE,
+    amount DECIMAL(8,2),
+    customer_id INT,
+    FOREIGN KEY(customer_id) REFERENCES customers(id) ON DELETE CASCADE
+);
+
+INSERT INTO customers (first_name, last_name, email) 
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
+       
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016/02/10', 99.99, 1),
+       ('2017/11/11', 35.50, 1),
+       ('2014/12/12', 800.67, 2),
+       ('2015/01/03', 12.50, 2),
+       ('1999/04/11', 450.25, 5);
+       
+SELECT * FROM customers, orders WHERE customers.id = orders.customer_id;
+
+SELECT * FROM customers JOIN orders ON customers.id = orders.customer_id;
+
+-- GET LIST OF CUSTOMERS ORDERED BY THE TOTAL AMOUNT
+SELECT customers.first_name, customers.last_name, sum(amount) as total_amount 
+FROM customers 
+JOIN orders 
+ON customers.id = orders.customer_id 
+GROUP BY customers.id 
+ORDER BY total_amount DESC;
+
+-- GET LIST OF CUSTOMERS ORDERED BY THE TOTAL AMOUNT, INCLUDING THE ONES WITHOUT ORDERS
+SELECT customers.first_name, customers.last_name, IFNULL(SUM(amount), 0) as total_amount 
+FROM customers 
+LEFT JOIN orders 
+ON customers.id = orders.customer_id 
+GROUP BY customers.id 
+ORDER BY total_amount DESC;
+
+SELECT * FROM customers LEFT JOIN orders ON customers.id = orders.customer_id;
+
+DELETE FROM customers WHERE id = 5;
+
+
